00b637edbf4273baf77f6daf57fe2dfd7c8f2631
Platon-Nikolaos Kiorpelidis <platwnace@gmail.com>
2016-03-21 22:00:07 +0200

    Initial Commit

---
+++ String Algorithms/my_string.h
@@ -0,0 +1,25 @@
+/******************************************************
+File implementation : my_string.h
+Authors             : Platon-Nikolaos Kiorpelidis
+Purpose             : my string management functions
+Date                : 21 Mar 2016
+*******************************************************/
+
+//replaces char_unwanted character from given string with char_replacer character.
+void replace_string_chars(char* string, char char_unwanted, char char_replacer);
+
+//removes the garbageChar from the given string.
+void remove_char(char* string, char garbageChar);
+
+//removes the extra spaces from a string.
+/*example:
+   test  test test
+would give:
+test test test*/
+void remove_extra_spaces(char* string);
+
+//returns a string from stdin buffer until a \n character is encountered.
+char* get_line();
+
+//uncapitalizes the characters of the given string.
+void uncapitalize(char* string);
